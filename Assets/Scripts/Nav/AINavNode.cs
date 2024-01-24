85d7e6b7f89a9b896f8cf2d959e1b2587c315445
Jacob Leonardo <leonardojacob257@gmail.com>
2024-01-24 16:59:34 -0700

    AStar

--- Assets/Scripts/Nav/AINavNode.cs
+++ Assets/Scripts/Nav/AINavNode.cs
@@ -22,7 +22,7 @@ public class AINavNode : MonoBehaviour
 		{
 			if (navPath.targetNode == this)
 			{
-				navPath.targetNode = GetRandomNeighbor();
+				navPath.targetNode = navPath.GetNextAINavNode(navPath.targetNode);
 			}
 		}
 	}
@@ -33,11 +33,10 @@ public class AINavNode : MonoBehaviour
 		{
 			if (navPath.targetNode == this)
 			{
-				navPath.targetNode = GetRandomNeighbor();
+				navPath.targetNode = navPath.GetNextAINavNode(navPath.targetNode);
 			}
 		}
 	}
-
 
 	#region HELPER_FUNCTIONS
 
@@ -67,6 +66,16 @@ public class AINavNode : MonoBehaviour
 	{
 		var nodes = GetAINavNodes();
 		return (nodes == null) ? null : nodes[Random.Range(0, nodes.Length)];
+	}
+
+	public static void ResetNodes()
+	{
+		var nodes = GetAINavNodes();
+		foreach (var node in nodes)
+		{
+			node.Parent = null;
+			node.Cost = float.MaxValue;
+		}
 	}
 
 	#endregion
